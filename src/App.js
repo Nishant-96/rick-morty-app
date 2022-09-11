c18e1919a3ddc35e5bb3d9ee9ee88e92bcfb789b
Nishant-96 <nishant11pratihast@gmail.com>
2022-09-11 18:12:20 +0530

    chore: route-setup

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
+import { Route, Routes } from "react-router-dom";
 import { Navbar } from "./components";
 import { ProfilePage } from "./pages";
-// import { Home } from "./pages";
+import { Home } from "./pages";
 
 function App() {
   return (
     <div className="">
       <Navbar />
-      <ProfilePage />
-      {/* <Home /> */}
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/profile" element={<ProfilePage />} />
+      </Routes>
     </div>
   );
 }
