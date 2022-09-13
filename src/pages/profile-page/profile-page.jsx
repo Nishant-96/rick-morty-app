b47900f20f80720320577081149b71e4bab2f899
Nishant-96 <nishant11pratihast@gmail.com>
2022-09-13 16:40:46 +0530

    feat: profile page of each character

--- src/pages/profile-page/profile-page.jsx
+++ src/pages/profile-page/profile-page.jsx
@@ -1,40 +1,124 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import {
+  getMultipleEpisodes,
+  getSingleCharacter,
+  getSingleLocation,
+} from "../../services";
 
 export function ProfilePage() {
+  const { profileId } = useParams();
+  const [profileCharacter, setProfileCharacter] = useState({
+    profile: {},
+    location: {},
+    origin: {},
+    episodesFeatured: [],
+  });
+
+  const episodeIdList = profileCharacter?.profile?.episode?.map((curr) => {
+    let strArr = curr.split("/").reverse();
+    return strArr[0];
+  });
+
+  // single character get call
+  useEffect(() => {
+    (async () => {
+      try {
+        const res = await getSingleCharacter(profileId);
+        setProfileCharacter((prev) => ({ ...prev, profile: res }));
+      } catch (error) {
+        console.error(error);
+      }
+    })();
+  }, [profileId]);
+
+  // location get call
+  useEffect(() => {
+    (async () => {
+      try {
+        const res = await getSingleLocation(
+          profileCharacter?.profile?.location?.name
+        );
+        const resTwo = await getSingleLocation(
+          profileCharacter?.profile?.origin?.name
+        );
+
+        if (res && resTwo) {
+          setProfileCharacter((prev) => ({
+            ...prev,
+            location: res?.results[0],
+            origin: resTwo?.results[0],
+          }));
+        }
+      } catch (error) {
+        console.error(error);
+      }
+    })();
+  }, [profileCharacter?.profile]);
+
+  //episodes get call
+  useEffect(() => {
+    (async () => {
+      try {
+        if (episodeIdList) {
+          const res = await getMultipleEpisodes(episodeIdList);
+          setProfileCharacter((prev) => ({
+            ...prev,
+            episodesFeatured: Array.isArray(res) ? res : [res],
+          }));
+        }
+      } catch (error) {
+        console.error(error);
+      }
+    })();
+  }, [profileCharacter?.profile]);
+
   return (
     <div className="mt-[60px] flex justify-center p-8">
       <div className="flex flex-col gap-3 md:flex-row md:gap-6">
         <div className="w-72">
           <img
-            src="https://i.annihil.us/u/prod/marvel/i/mg/3/d0/60464e1695be7/clean.jpg"
+            src={profileCharacter?.profile?.image}
             alt="character image"
             className="w-full h-auto object-cover rounded"
           />
         </div>
-        {/* <div className="bg-green-500">Alive</div> */}
         <div>
-          <h2 className="text-2xl text-center">Character Name</h2>
+          <h2 className="text-2xl text-center">
+            {profileCharacter?.profile?.name}
+          </h2>
           <div className="flex flex-col p-2 gap-1">
             <div>
-              <b>Species:</b> Something
+              <b>Species:</b> {profileCharacter?.profile?.species}
             </div>
             <div>
-              <b>Gender: </b>Male
+              <b>Gender: </b>
+              {profileCharacter?.profile?.gender}
             </div>
+            {profileCharacter?.profile?.type && (
+              <div>
+                <b>Type: </b>
+                {profileCharacter?.profile?.type}
+              </div>
+            )}
             <div>
               <div className="font-bold underline">Location: </div>
               <div className="flex flex-col justify-center ml-10">
                 <div>
-                  <b>Name:</b>Earth
+                  <b>Name: </b>
+                  {profileCharacter?.profile?.location?.name}
                 </div>
                 <div>
-                  <b>Dimension: </b>C137
+                  <b>Dimension: </b>
+                  {profileCharacter?.location?.dimension}
                 </div>
                 <div>
-                  <b>Type: </b>C137
+                  <b>Type: </b>
+                  {profileCharacter?.location?.type}
                 </div>
                 <div>
-                  <b>Total Residents: </b>1000
+                  <b>Total Residents: </b>
+                  {profileCharacter?.location?.residents?.length}
                 </div>
               </div>
             </div>
@@ -42,19 +126,38 @@ export function ProfilePage() {
               <div className="font-bold underline">Origin: </div>
               <div className="flex flex-col justify-center ml-10">
                 <div>
-                  <b>Name:</b>Earth
+                  <b>Name: </b>
+                  {profileCharacter?.profile?.origin?.name}
                 </div>
                 <div>
-                  <b>Dimension: </b>C137
+                  <b>Dimension: </b>
+                  {profileCharacter?.origin?.dimension}
                 </div>
                 <div>
-                  <b>Type: </b>C137
+                  <b>Type: </b>
+                  {profileCharacter?.origin?.type}
                 </div>
                 <div>
-                  <b>Total Residents: </b>1000
+                  <b>Total Residents: </b>
+                  {profileCharacter?.origin?.residents?.length}
                 </div>
               </div>
             </div>
+            {profileCharacter?.episodesFeatured?.length > 0 && (
+              <div>
+                <div className="font-bold underline">Episodes Featured: </div>
+                <div className="flex flex-col justify-center ml-10">
+                  {profileCharacter?.episodesFeatured
+                    ?.slice(0, 10)
+                    .map((currEpi, index) => (
+                      <div key={currEpi.id} className="flex gap-3">
+                        <div>{index + 1}.</div>
+                        <div>{currEpi.name}</div>
+                      </div>
+                    ))}
+                </div>
+              </div>
+            )}
           </div>
         </div>
       </div>
---
+++ src/services/index.js
@@ -0,0 +1,5 @@
+export { getAllCharacters } from "./getCharacter";
+export { getSingleCharacter } from "./getCharacter";
+export { getSingleLocation } from "./getLocation";
+export { getPerPageCharacters } from "./getCharacter";
+export { getMultipleEpisodes } from "./getEpisodes";
