78b1ec07b285317aa984182b99a174785c675bb4
Nishant-96 <nishant11pratihast@gmail.com>
2022-09-13 17:34:31 +0530

    refactor: scroll to top, route to home from profile page

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
-import { Route, Routes } from "react-router-dom";
+import { useEffect } from "react";
+import { Route, Routes, useLocation } from "react-router-dom";
 import { Navbar } from "./components";
 import { ProfilePage } from "./pages";
 import { Home } from "./pages";
 
 function App() {
+  const { pathname } = useLocation();
+  
+  // scroll to top on route change
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
+
   return (
     <div className="h-screen">
       <Navbar />
--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -1,9 +1,12 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 export function Navbar() {
   return (
     <div className="w-full bg-slate-50 p-4 fixed top-0 left-0 right-0 z-10">
-      <div className="font-bold text-lg text-center">Rick and Morty</div>
+      <Link to="/">
+        <div className="font-bold text-lg text-center">Rick and Morty</div>
+      </Link>
     </div>
   );
 }
